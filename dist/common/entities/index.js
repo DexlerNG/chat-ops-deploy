7126e88396ec000e69919b792c4470062d8ed034
Micheal Akinwonmi <michealakinwonmi@gmail.com>
2026-06-05 23:08:53 +0100

    changing to github

--- dist/common/entities/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.FunctionResponseDTO = void 0;
-class FunctionResponseDTO {
-}
-exports.FunctionResponseDTO = FunctionResponseDTO;
-//# sourceMappingURL=index.js.map
